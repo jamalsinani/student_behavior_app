b2047f0c384a379c7cdfcec5fc567130df519dba
Jamal Sinani <jamalsinani@gmail.com>
2026-03-12 13:17:40 +0400

    التعديل

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'firebase_options.dart';
 
 import 'core/app_theme.dart';
 import 'screens/school_home_screen.dart';
+import 'package:flutter/foundation.dart';
+
+Future<void> initNotifications() async {
+
+  if (kIsWeb) {
+    print("Notifications disabled on Web");
+    return;
+  }
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+
+    try {
+
+      String? apnsToken = await messaging.getAPNSToken();
+      print("APNS TOKEN: $apnsToken");
+
+      String? fcmToken = await messaging.getToken();
+      print("FCM TOKEN: $fcmToken");
+
+    } catch (e) {
+      print("FCM ERROR: $e");
+    }
+
+  }
+
+}
 
 Future<void> main() async {
 
@@ -13,6 +48,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await initNotifications();
 
   runApp(const StudentBehaviorApp());
 }
@@ -25,11 +62,6 @@ class StudentBehaviorApp extends StatefulWidget {
 }
 
 class _StudentBehaviorAppState extends State<StudentBehaviorApp> {
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
